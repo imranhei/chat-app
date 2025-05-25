9c2232e3cca88ff7856b166125b511b62eb99ae4
Imran Hossain <imranhossainrana.2014@gmail.com>
2025-05-25 12:33:04 +0600

    change mongoose connection

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { app, server } from "./lib/socket.js"; // Import the app from socket.js
+import mongoose from "mongoose";
 
-import { connectDB } from "./lib/db.js";
+// import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -11,6 +12,11 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 const allowedOrigins = [
   process.env.CLIENT_URL,
@@ -48,5 +54,5 @@ app.use("/api/messages", messageRoutes);
 
 server.listen(PORT, () => {
   console.log("Server is running on PORT:", PORT);
-  connectDB();
+  // connectDB();
 });
